5f78484d30ee52302e90d20f01ced44dcf6017bf
Stephanie Dong <stephaniexd11@gmail.com>
2018-03-16 19:08:11 -0700

    only print wrong

--- code/qa_model_pointer.py
+++ code/qa_model_pointer.py
@@ -428,7 +428,7 @@ class QAModel(object):
                 em_total += em
 
                 # Optionally pretty-print
-                if print_to_screen:
+                if print_to_screen and em < 1:
                     print_example(self.word2id, batch.context_tokens[ex_idx], batch.qn_tokens[ex_idx], batch.ans_span[ex_idx, 0], batch.ans_span[ex_idx, 1], pred_ans_start, pred_ans_end, true_answer, pred_answer, f1, em)
 
                 if num_samples != 0 and example_num >= num_samples:
@@ -442,6 +442,9 @@ class QAModel(object):
 
         toc = time.time()
         logging.info("Calculating F1/EM for %i examples in %s set took %.2f seconds" % (example_num, dataset, toc-tic))
+        if print_to_screen:
+            logging.info(
+                "F1: %2f, EM: %.2f" % (f1_total, em_total))
 
         return f1_total, em_total
 
